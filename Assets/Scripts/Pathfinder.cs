20bf62f57ddfc0ade4c7f581c04f23ef5d1c6a6c
QuentinLB <q.lebour@gmail.com>
2015-10-19 17:19:32 +0200

    first commit

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Pathfinder : MonoBehaviour {
+
+    public Transform target;
+    Vector3 storeTarget;
+    bool savePos;
+    bool overrideTarget;
+
+    Vector3 acceleration;
+    Vector3 velocity;
+    public float maxSpeed = 5f;
+    float storeMaxSpeed;
+    float targetSpeed;
+
+    Rigidbody rigidBody;
+
+    public List<Vector3> EscapeDirections = new List<Vector3>();
+
+	// Use this for initialization
+	void Start () {
+        storeMaxSpeed = maxSpeed;
+        targetSpeed = storeMaxSpeed;
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        Debug.DrawLine(transform.position, target.position);
+        Vector3 forces = MoveTowardsTarget(target.position);
+        acceleration = forces;
+        velocity += 2 * acceleration * Time.deltaTime;
+        if (velocity.magnitude > maxSpeed)
+        {
+            velocity = velocity.normalized * maxSpeed;
+        }
+
+        rigidBody.velocity = velocity;
+
+        Quaternion desiredRotation = Quaternion.LookRotation(velocity);
+        transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, Time.deltaTime * 2);
+    }
+
+    Vector3 MoveTowardsTarget(Vector3 target)
+    {
+        Vector3 distance = target - transform.position;
+        if (distance.magnitude < 25)
+        {
+            return distance.normalized * -maxSpeed;
+        }
+        else
+        {
+            return distance.normalized * maxSpeed;
+        }
+    }
+
+    RaycastHit[] Rays(Vector3 direction, float offsetX)
+    {
+        Ray ray = new Ray(transform.position + new Vector3(offsetX, 0, 0), direction);
+        Debug.DrawRay(transform.position + new Vector3(offsetX, 0, 0), direction*10*maxSpeed, Color.red);
+
+        float distanceToLookAhead = maxSpeed * 10;
+        RaycastHit[] hits = Physics.SphereCastAll(ray, 5, distanceToLookAhead);
+
+        return hits;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(h, 0.0f, v);
+        rb.AddForce(movement * speed);
+	}
+}
